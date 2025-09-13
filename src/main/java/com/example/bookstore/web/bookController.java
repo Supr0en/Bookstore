f6bc8ebb015a5d5bf341d529184c6557d6d3e76e
Supr0en <erik.ruotsalainen1@gmail.com>
2025-09-13 21:21:54 +0300

    C3.2 Bookstore: Listpage

--- src/main/java/com/example/bookstore/web/bookController.java
+++ src/main/java/com/example/bookstore/web/bookController.java
@@ -1,12 +1,28 @@
 package com.example.bookstore.web;
 
+import com.example.bookstore.domain.Book;
+import java.util.List;
+import com.example.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class bookController {
+    @Autowired
+    BookRepository bookRepository;
+
     @GetMapping("/index")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String booklist(Model model){
+        List<Book> booksList =  (List<Book>) bookRepository.findAll();
+        model.addAttribute("booklist", booksList);
+        return "booklist";
     }
 }
