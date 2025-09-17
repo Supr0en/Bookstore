6d7158121732cbec9f661e526d37a4a4467ab899
Supr0en <erik.ruotsalainen1@gmail.com>
2025-09-17 12:35:08 +0300

    C4.4 Book from with category dropdown

--- src/main/java/com/example/bookstore/web/bookController.java
+++ src/main/java/com/example/bookstore/web/bookController.java
@@ -4,6 +4,7 @@ import com.example.bookstore.domain.Book;
 import java.util.List;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class bookController {
     private final BookRepository bookRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public bookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -32,6 +35,7 @@ public class bookController {
     @RequestMapping(value = "/addBook")
     public String addBook(Model model){
         model.addAttribute("book", new Book());
+        model.addAttribute("categoryList", categoryRepository.findAll());
         return "addbook";
     }
     @RequestMapping(value = "/saveBook", method = RequestMethod.POST)
